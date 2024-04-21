e10b03213dd22d7b47cd6bf60ef1fe07093ecb38
shadowystupidcoder <154037987+shadowystupidcoder@users.noreply.github.com>
2024-04-21 01:07:29 -0700

    Update sniper.mjs

--- sniper.mjs
+++ sniper.mjs
@@ -23,15 +23,39 @@ console.log("swapped in tx id:", tx)
 
 // 2. getting all the pool keys
 const getAta = async (mint, publicKey) => PublicKey.findProgramAddressSync([publicKey.toBuffer(), spl.TOKEN_PROGRAM_ID.toBuffer(), mint.toBuffer()], spl.ASSOCIATED_TOKEN_PROGRAM_ID)[0];
-export const getKeys = async (marketId, baseDecimals, quoteDecimals) => {
-  const marketInfo = await Market.getLayout(new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX')).decode((await connection.getAccountInfo(marketId)).data);
-  const [baseMint, quoteMint] = [marketInfo.baseMint, marketInfo.quoteMint];
-  const [ownerBaseAta, ownerQuoteAta] = await Promise.all([getAta(baseMint, wallet.publicKey), getAta(quoteMint, wallet.publicKey)]);
-  const authority = PublicKey.findProgramAddressSync([Buffer.from([97, 109, 109, 32, 97, 117, 116, 104, 111, 114, 105, 116, 121])], ray)[0];
-  const marketAuthority = PublicKey.createProgramAddressSync([marketId.toBuffer(), Buffer.from([Number(marketInfo.vaultSignerNonce.toString())]), Buffer.alloc(7)], new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'));
-  const seeds = ['amm_associated_seed', 'coin_vault_associated_seed', 'pc_vault_associated_seed', 'lp_mint_associated_seed', 'temp_lp_token_associated_seed', 'target_associated_seed', 'withdraw_associated_seed', 'open_order_associated_seed', 'pc_vault_associated_seed'].map(seed => Buffer.from(seed, 'utf-8'));
-  const [id, baseVault, coinVault, lpMint, lpVault, targetOrders, withdrawQueue, openOrders, quoteVault] = await Promise.all(seeds.map(seed => PublicKey.findProgramAddress([ray.toBuffer(), marketId.toBuffer(), seed], ray)));
-  return {
+const getKeys = async (marketId, baseDecimals, quoteDecimals) => {
+async function getMarketInfo(marketId) {
+const info = await connection.getAccountInfo(marketId)
+const ownAddress = new PublicKey(info.data.slice(13, 45))
+const vaultSignerNonce = new NearUInt64().decode(new Uint8Array((info).data.subarray(45, 53)))
+console.log(vaultSignerNonce)
+const baseMint = new PublicKey(info.data.slice(53, 85))
+const quoteMint = new PublicKey(info.data.slice(85, 117))
+const bids = new PublicKey(info.data.slice(285, 317))
+const asks = new PublicKey(info.data.slice(317, 349))
+const event = new PublicKey(info.data.slice(253, 285))
+const baseVault = new PublicKey(info.data.slice(117, 149))
+const quoteVault = new PublicKey(info.data.slice(165, 197))
+const marketInfo = {
+ownAddress,
+vaultSignerNonce,
+baseMint,
+quoteMint,
+bids,
+asks,
+event,
+baseVault,
+quoteVault}
+return(marketInfo)
+}
+const marketInfo = await getMarketInfo(marketId)
+const [baseMint, quoteMint] = [marketInfo.baseMint, marketInfo.quoteMint];
+const [ownerBaseAta, ownerQuoteAta] = await Promise.all([getAta(baseMint, wallet.publicKey), getAta(quoteMint, wallet.publicKey)]);
+const authority = PublicKey.findProgramAddressSync([Buffer.from([97, 109, 109, 32, 97, 117, 116, 104, 111, 114, 105, 116, 121])], ray)[0];
+const marketAuthority = PublicKey.createProgramAddressSync([marketId.toBuffer(), Buffer.from([Number(marketInfo.vaultSignerNonce.toString())]), Buffer.alloc(7)], new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'));
+const seeds = ['amm_associated_seed', 'coin_vault_associated_seed', 'pc_vault_associated_seed', 'lp_mint_associated_seed', 'temp_lp_token_associated_seed', 'target_associated_seed', 'withdraw_associated_seed', 'open_order_associated_seed', 'pc_vault_associated_seed'].map(seed => Buffer.from(seed, 'utf-8'));
+const [id, baseVault, coinVault, lpMint, lpVault, targetOrders, withdrawQueue, openOrders, quoteVault] = await Promise.all(seeds.map(seed => PublicKey.findProgramAddress([ray.toBuffer(), marketId.toBuffer(), seed], ray)));
+return {
     programId: ray,
     baseMint,
     quoteMint,
